47d371f1efcd29dece930ef7f9f5304f23695c0a
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2021-12-21 10:17:02 +0100

    21

---
+++ minimofamiliar.js
@@ -0,0 +1,12 @@
+const arrayMinDescendientes = [
+    [1, 2400],
+    [2, 2700],
+    [3, 4000],
+    [4, 4500],
+  ];
+  function minimoFamiliar(descendientes) {
+      return minimo;
+  }
+  for (i=1; i<=10; i++){
+      console.log(`Número de descendientes: ${i} es: ${minimoFamiliar(i)}`);
+  }
