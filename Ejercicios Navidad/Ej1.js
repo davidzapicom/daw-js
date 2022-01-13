0ce92d639d0015b2e81adf78b655a332220c0e6c
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2022-01-13 12:55:27 +0100

    13

--- Ejercicios Navidad/Ej1.js
+++ Ejercicios Navidad/Ej1.js
@@ -1,68 +1,288 @@
-const arrayMinDescendientes = [
+var diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+var semanas = [
+    "Semana 1",
+    ["06/09/2021, 12/09/2021"],
     [
-        "Semana1",
-        [06/09/2021,12/09/2021],
-        [
-            [1,0],
-            [2,2500],
-            [3,4500],
-            [4,5000],
-            [5,3000],
-            [6,2500],
-            [7,8000],
-        ]
-    ],
+        [1, 0],
+        [2, 2500],
+        [3, 4500],
+        [4, 5000],
+        [5, 3000],
+        [6, 2500],
+        [7, 8000]
+    ]
+    ,
+    "Semana 2",
+    ["13/09/2021, 19/09/2021"],
     [
-        "Semana2",
-        [13/09/2021,19/09/2021],
-        [
-            [1,0],
-            [2,2500],
-            [3,4500],
-            [4,5000],
-            [5,3000],
-            [6,2500],
-            [7,8000],
-        ]
+        [1, 0],
+        [2, 3500],
+        [3, 5800],
+        [4, 2000],
+        [5, 2000],
+        [6, 1500],
+        [7, 3000]
     ],
+    "Semana 3",
+    ["20/09/2021, 26/09/2021"],
     [
-        "Semana3",
-        [06/09/2021,12/09/2021],
-        [
-            [1,0],
-            [2,2500],
-            [3,4500],
-            [4,5000],
-            [5,3000],
-            [6,2500],
-            [7,8000],
-        ]
+        [1, 0],
+        [2, 4500],
+        [3, 3800],
+        [4, 1000],
+        [5, 2000],
+        [6, 3500],
+        [7, 3000]
     ],
+    "Semana 4",
+    ["27/09/2021, 03/10/2021"],
     [
-        "Semana4",
-        [06/09/2021,12/09/2021],
-        [
-            [1,0],
-            [2,2500],
-            [3,4500],
-            [4,5000],
-            [5,3000],
-            [6,2500],
-            [7,8000],
-        ]
+        [1, 0],
+        [2, 2500],
+        [3, 1800],
+        [4, 3000],
+        [5, 4000],
+        [6, 2500],
+        [7, 1000]
     ],
+    "Semana 5",
+    ["04/10/2021, 10/10/2021"],
     [
-        "Semana5",
-        [06/09/2021,12/09/2021],
-        [
-            [1,0],
-            [2,2500],
-            [3,4500],
-            [4,5000],
-            [5,3000],
-            [6,2500],
-            [7,8000],
-        ]
-    ],
-
-  ];
+        [1, 0],
+        [2, 3500],
+        [3, 2800],
+        [4, 5000],
+        [5, 1000],
+        [6, 2500],
+        [7, 1000]
+    ]
+];
+function LanzarPrograma() {
+    var result = [];
+    result.push(maximoyminimosemana());
+    result.push(ventasDomingoSuperiorMedia());
+    result.push(ventastotalesymedia());
+    result.push(rankingVentastotalesymedia());
+    result.push(diferencial());
+    var msg = "";
+    msg += "Info 1 - Días de mayor y menor venta, \n";
+    result[0].forEach(element => {
+        msg += "\t[" + element + "]\n";
+    });
+    msg += "Info 2 - Domingo supera a media, \n";
+    result[1].forEach(element => {
+        msg += "\t[" + element + "]\n";
+    });
+    msg += "Info 3 - Ventas totales y medias diarias, \n";
+    result[2].forEach(element => {
+        msg += "\t[" + element + "]\n";
+    });
+    msg += "Info 4 - Ranking ventas semanales, \n";
+    //alert(result[3]);
+    result[3].forEach(element => {
+        msg += "\t[" + element + "]\n";
+    });
+    msg += "Info 5 - Diferencial entre ventas media, \n";
+    result[4].forEach(element => {
+        msg += "\t[" + element + "]\n";
+    });
+    alert(msg);
+}
+function maximoyminimosemana() {
+    var maximo = 0;
+    var minimo = Infinity;
+    var diaMaximo = 0;
+    var diaMinimo = 0;
+    var msg = '';
+    var numerosSemana = [];
+    var j = 0;
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                if (maximo < semanas[i][k][1]) {
+                    maximo = semanas[i][k][1];
+                    diaMaximo = semanas[i][k][0];
+                }
+                if (minimo > semanas[i][k][1]) {
+                    minimo = semanas[i][k][1];
+                    diaMinimo = semanas[i][k][0];
+                }
+            }
+        }
+        var datos = [diaMaximo, maximo, diaMinimo, minimo, semanas[j]];
+        numerosSemana.push(datos);
+        maximo = 0;
+        minimo = Infinity;
+        diaMaximo = 0;
+        diaMinimo = 0;
+        j += 3;
+    }
+    var cuentaDias = 0;
+    var cuentasSemanas = 1;
+    var datos = [];
+    for (let l = 0; l < numerosSemana.length; l++) {
+        var dato = [[semanas[cuentaDias], semanas[cuentasSemanas], [numerosSemana[l][0]], [numerosSemana[l][2]]]];
+        datos.push(dato);
+        cuentaDias += 3;
+        cuentasSemanas += 3;
+        // msg += numerosSemana[l][4] + '\n';
+        // msg += "El dia que mas ha vendido es el " + diasSemana[numerosSemana[l][0] - 1] + " con un total de " + numerosSemana[l][1];
+        // msg += "\nEl dia que menos ha vendido es el " + diasSemana[numerosSemana[l][2] - 1] + " con un total de " + numerosSemana[l][3] + "\n";
+    }
+    //alert(msg);
+    return datos;
+}
+function ventasDomingoSuperiorMedia() {
+    var sumatorio = 0;
+    var isSuperior = false;
+    var media = 0;
+    var msg = '';
+    var numerosSemana = [];
+    var j = 0;
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                sumatorio += semanas[i][k][1];
+            }
+            if (semanas[i][k][0] == 7) {
+                media = sumatorio / 7;
+                if (media > semanas[i][k][1]) {
+                    isSuperior = false;
+                }
+                else {
+                    isSuperior = true;
+                }
+            }
+        }
+        var datos = [isSuperior, semanas[j]];
+        numerosSemana.push(datos);
+        isSuperior = false;
+        sumatorio = 0;
+        media = 0;
+        j += 3;
+    }
+    var cuentaDias = 0;
+    var cuentasSemanas = 1;
+    var datos = [];
+    for (let l = 0; l < numerosSemana.length; l++) {
+        var dato = [[semanas[cuentaDias], semanas[cuentasSemanas], [numerosSemana[l][0]]]];
+        datos.push(dato);
+        cuentaDias += 3;
+        cuentasSemanas += 3;
+        // msg += "Para la " + numerosSemana[l][1] + " las ventas se superan: " + numerosSemana[l][0] + '\n';
+    }
+    //alert(msg);
+    return datos;
+}
+function ventastotalesymedia() {
+    var media = [0, 0, 0, 0, 0, 0, 0];
+    var msg = '';
+    var numerosSemana = [0, 0, 0, 0, 0, 0, 0];
+    var j = 0;
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                numerosSemana[k] += semanas[i][k][1];
+            }
+        }
+        j += 3;
+    }
+    for (let m = 0; m < media.length; m++) {
+        media[m] = Math.round(numerosSemana[m] / 7);
+    }
+    var datos = [];
+    for (let l = 0; l < numerosSemana.length; l++) {
+        var dato = [l + 1, [numerosSemana[l], [media[l]]]];
+        datos.push(dato);
+        //msg += "Los " + diasSemana[l] + " las ventas han sido: " + numerosSemana[l] + '\n';
+        //msg += "Los " + diasSemana[l] + " con la media siguiente fueron: " + media[l] + '\n';
+    }
+    //alert(msg);
+    return datos;
+}
+function rankingVentastotalesymedia() {
+    var media = [0, 0, 0, 0, 0, 0, 0];
+    var msg = '';
+    var numerosSemana = [0, 0, 0, 0, 0, 0, 0];
+    var copiaNumeroSemanas = [];
+    var diaMaximo = 0;
+    var orden = []; //["a",ventas totales, ventas media]
+    var ventasTotalesMaximo = 0;
+    var j = 0;
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                numerosSemana[k] += semanas[i][k][1];
+            }
+        }
+        j += 3;
+    }
+    copiaNumeroSemanas = numerosSemana;
+    for (let m = 0; m < media.length; m++) {
+        media[m] = Math.round(numerosSemana[m] / 7);
+    }
+    for (let i = 0; i < copiaNumeroSemanas.length; i++) {
+        for (let m = 0; m < copiaNumeroSemanas.length; m++) {
+            if (copiaNumeroSemanas[m] > ventasTotalesMaximo) {
+                ventasTotalesMaximo = copiaNumeroSemanas[m];
+                diaMaximo = m;
+            }
+        }
+        var datos = [diasSemana[diaMaximo], numerosSemana[diaMaximo], media[diaMaximo]];
+        orden.push(datos);
+        copiaNumeroSemanas[diaMaximo] = 0;
+        ventasTotalesMaximo = 0;
+    }
+    var datos = [];
+    for (let i = 0; i < orden.length - 1; i++) {
+        //msg += "En la " + (i + 1) + "ª posicion tenemos:\n";
+        //msg += "    El dia " + orden[i][0] + " con un total de ventas de " + orden[i][1] + " y una media de " + orden[i][2] + ".\n";
+        datos.push = orden[i];
+    }
+    //alert(msg);
+    return datos;
+}
+function diferencial() {
+    var media = [0, 0, 0, 0, 0, 0, 0];
+    var msg = '';
+    var numerosSemana = [0, 0, 0, 0, 0, 0, 0];
+    var sumatorio = 0;
+    var mediaAux = 0;
+    var mediaSemana = 0;
+    var j = 0;
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                sumatorio += semanas[i][k][1];
+            }
+            if (semanas[i][k][0] == 7) {
+                mediaAux = sumatorio / 7;
+                if (mediaAux > mediaSemana) {
+                    mediaSemana = mediaAux;
+                }
+            }
+        }
+        sumatorio = 0;
+    }
+    for (let i = 2; i < semanas.length; i = i + 3) {
+        for (let k = 0; k < semanas[2].length; k++) {
+            if (semanas[i][k][0] != 1) {
+                numerosSemana[k] += semanas[i][k][1];
+            }
+        }
+        j += 3;
+    }
+    for (let m = 0; m < media.length; m++) {
+        media[m] = Math.round(numerosSemana[m] / 7);
+    }
+    mediaSemana = Math.round(mediaSemana);
+    for (let l = 0; l < numerosSemana.length; l++) {
+        var datos = [];
+        var dato = ["Info 5 - diferencial", [semanas[j], semanas[3], [diasSemana[l]] - media[l]]];
+        datos.push = (dato);
+        //msg += "La media de ventas de los " + diasSemana[l] + " fue de : " + media[l] + '\n';
+        //msg += "Con una diferencia respecto a la mejor media de las semanas de " + (mediaSemana - media[l]) + '\n';
+    }
+    return datos;
+    //alert(msg);
+}
--- Ejercicios Navidad/Ej2.js
+++ Ejercicios Navidad/Ej2.js
@@ -1,6 +1,4 @@
-var datos = [];
-
-var tarjetas = [
+const tarjetas = [
     [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8],
     [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9],
     [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6],
@@ -19,87 +17,90 @@ var tarjetas = [
     [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6],
     [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3]
 ];
-
-var entidades = [
-    [3, 'Amex (American Express)'],
-    [4, 'Visa'],
-    [5, 'Mastercard'],
-    [6, 'Otra']
+const entidades = [
+    [3, "Amex (American Express)"],
+    [4, "Visa"],
+    [5, "Mastercard"],
+    [6, "Otra"]
 ];
-
+const arrayTarjetas = [];
 const final = [
     [3, 0, 0, ""],
     [4, 0, 0, ""],
     [5, 0, 0, ""],
-    [6, 0, 0, ""],
+    [6, 0, 0, ""]
 ];
-
-function tarjetavalida(tarjeta) {
-    var j = 0;
-    var suma = 0;
-    for (var i = tarjeta.length-1; i >= 0; i--) {
-        if (j % 2 == 0) {
-            suma += tarjeta[i];
-        } else {
-            let multiplicador =  tarjeta[i] * 2;
-            suma += (multiplicador > 9)? multiplicador-9: tarjeta[i];
+var msg = "TARJETAS\n";
+function tarjetaValida(tarjeta) {
+    var sumaimpar = 0;
+    var sumapar = 0;
+    var total;
+    var impar;
+    var valido = false;
+    for (let x = 0; x < tarjeta.length; x++) {
+        if ((x + 1) % 2 != 0) {
+            impar = tarjeta[x] * 2;
+            if (impar > 9) {
+                impar = impar - 9;
+            }
+            sumaimpar += impar;
         }
-        console.log(tarjeta[i]);
-        j++;
-    }
-    return (suma % 10 == 0);
-}
-
-tarjetavalida(tarjetas[1]);
-function val_tarjetas(tarjetas) {
-    let tarjetasProcesadas = [];
-    for (var i = 0; i < tarjetas.length; i++) {
-        var tarjeta = tarjetas[i];
-        var validacion = tarjetavalida(tarjeta);
-        let indice = entidades.findIndex(elemento => elemento[0] === tarjeta[0]);
-        let nombreEntidad = (indice == -1)?"Error: no existe entidad":entidades[indice][1];
-        tarjetasProcesadas.push([nombreEntidad, tarjeta.join(''), validacion]);
-    }
-    return tarjetasProcesadas;
-}
-
-function infoent(tarjetas) {
-    for (let i = 0; i < tarjetas.length; i++) {
-        switch (datos[i][0]) {
-            case 3:
-                final[0][1]++;
-                if (!datos[i][2]) {
-                    final[0][2]++;
-                }
-                break;
-            case 4:
-                final[1][1]++;
-                if (!datos[i][2]) {
-                    final[1][2]++;
-                }
-                break;
-            case 5:
-                final[2][1]++;
-                if (!datos[i][2]) {
-                    final[2][2]++;
-                }
-                break;
-            case 6:
-                final[3][1]++;
-                if (!datos[i][2]) {
-                    final[3][2]++;
-                }
-                break;
+        else {
+            sumapar += tarjeta[x];
         }
     }
-    for (let i = 0; i < final.length; i++) {
-        if (final[i][2] == 0) {
-            final[i][3] = 0 + " %";
-        } else {
-            final[i][3] = (final[i][2] / final[i][1] * 100) + "%";
+    total = sumaimpar + sumapar;
+    if (total % 10 == 0) {
+        valido = true;
+    }
+    return valido;
+}
+ValidacionTodasTarjetas(tarjetas);
+function ValidacionTodasTarjetas(tarjetas) {
+    for (let y = 0; y < tarjetas.length; y++) {
+        var tarjeta = tarjetas[y];
+        var validar = tarjetaValida(tarjetas[y]);
+        var entidad = tarjeta[0];
+        var tarjetatexto = "";
+        for (let j = 0; j < entidades.length; j++) {
+            if (entidades[j][0] == entidad) {
+                tarjetatexto = entidades[j][1];
+            }
+        }
+        let datos = [entidad, tarjetatexto, validar];
+        // arrayTarjetas[y][0] = entidad;
+        // arrayTarjetas[y][1] = tarjeta.join('');
+        // arrayTarjetas[y][2] = validar;
+        arrayTarjetas.push(datos);
+    }
+    arrayTarjetas.forEach(element => {
+        msg += element + "\n";
+    });
+    return arrayTarjetas;
+}
+Entidades(arrayTarjetas);
+function Entidades(arrayTarjetas) {
+    for (let x = 0; x < arrayTarjetas.length; x++) {
+        for (let i = 0; i < final.length; i++) {
+            if (arrayTarjetas[x][0] == final[i][0]) {
+                final[i][1]++;
+                if (!arrayTarjetas[x][2]) {
+                    final[i][2]++;
+                }
+            }
         }
     }
-    console.log(final);
+    for (let x = 0; x < final.length; x++) {
+        if (final[x][2] == 0) {
+            final[x][3] = 0 + " %";
+        }
+        else {
+            final[x][3] = (final[x][2] / final[x][1]) * 100 + "%";
+        }
+    }
+    msg += "\nTARJETAS POR ENTIDAD \n";
+    final.forEach(element => {
+        msg += element + "\n";
+    });
 }
-
-infoent(tarjetas);
+alert(msg);
