70194f3f570f9fa61bd1f3c03fd6524dd3191d51
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2022-01-19 14:21:25 +0100

    19

--- T2/02-objetos.js
+++ T2/02-objetos.js
@@ -19,3 +19,52 @@ let array2 = array1;
 
 array2[2] = 10;
 console.log(array1[2]);
+
+console.log(cocheNuria === cochePaula);
+
+cocheNuria = {};
+
+for (propiedad in cochePaula) {
+    cocheNuria[propiedad] = cochePaula[propiedad];
+}
+
+//Utilizando el método assign para copiar propiedades
+Object.assign(cocheNuria, cochePaula);
+
+//Creamos un motor
+let motorOpelCorsa2000 = {
+    combustible : 'Gasolina',
+    centimetrosCubicos : 2000,
+    cilindros : 4,
+    turbo : false,
+};
+
+cochePaula.motor = motorOpelCorsa2000;
+let cocheVictor = {};
+//El metodo no copialos datos de los objetos incluidos
+Object.assign(cocheVictor, cochePaula);
+
+cocheVictor.motor.centimetrosCubicos = 3000;
+cocheVictor.motor.turbo = true;
+cocheVictor.color = 'negro';
+
+//Método para mostrar coche
+cochePaula.mostrarCoche = function() { 
+    for (propiedad in this) {
+        console.log(this[propiedad]);
+    }
+}
+
+//Método para actualizar km
+cochePaula.actualizarKm = function(nuevoKilometraje) {
+    this.kilometros = nuevoKilometraje;
+}
+
+cochePaula.mostrarCoche();
+cocheSara.actualizarKm(90000);
+let cocheSara = {};
+
+Object.assign(cocheSara,cochePaula);
+
+cocheSara.color = 'Rojo';
+cocheSara.mostrarCoche();
