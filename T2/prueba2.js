de5f661d0f655046260bba26d31fdc8baea38408
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2022-01-19 13:26:03 +0100

    19

--- T2/prueba2.js
+++ T2/prueba2.js
@@ -1,17 +1,20 @@
 //OBJETOS
 //Sintáxis literal
+let objetos = {};
 
-let objetos{};
-
-let cochePaula {
-
-}
-
+//definicion de los atributos del coche de paula
+let cochePaula = {
+    marca : "Opel",
+    modelo : "Corsa",
+    matricula : "U-000-NOSE",
+    color : "blanco perlado",
+    kilometros : 154256,
+    fechaMatriculacion : 2007,
+};
 
 console.log(cochePaula.matricula);
 cochePaula.combustible = "Gasolina";
 cochePaula.caballos = undefined;
-
 console.log(cochePaula.motorizacion); //undefined
 console.log(cochePaula.combustible);
 console.log(cochePaula.caballos);
@@ -25,6 +28,21 @@ cochePaula["motorizacion"] = "TI 2000CC";
 
 //Propiedades "variables"...
 let nuevaPropiedad = "numero de puertas";
-
 cochePaula[nuevaPropiedad] = 5;
 
+//Obtener propiedades de un objeto
+for (propiedad in cochePaula) {
+    console.log(`propiedad ${propiedad} es: ${cochePaula[propiedad]}`);
+}
+
+//Hacer objetos a partir de una función
+function nuevoCoche(nombrePropietario, matricula, marca, modelo) {
+    return {
+        nombrePropietario,
+        matricula,
+        marca,
+        modelo
+    };
+};
+
+let cocheDani = nuevoCoche("Dani", "O-234-BB", "Seat", "Ibiza");
