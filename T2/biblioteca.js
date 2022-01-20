56e7991aa8dab7b7bc7104580912478f3865aed6
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2022-01-20 14:26:48 +0100

    20

--- T2/biblioteca.js
+++ T2/biblioteca.js
@@ -1,10 +1,21 @@
-function Biblioteca(, , , ) {
-    this. = ;
-    this. = ;
-    this. = ;
-
-
+function Biblioteca(nombre, direccion, correoElectronico, telefono, socios) {
+    this.nombre = nombre;
+    this.direccion = direccion;
+    this.correoElectronico = correoElectronico;
+    this.telefono = telefono;
+    this.socios = socios;
 }
 
+const biblioteca1 = new Biblioteca("Biblioteca de Avilés", "Plaza Domingo Álvarez Acebal, 2, 33402. Avilés", "bibliotecabances@aviles.es", 985510439, 25216);
 
-function Titulos(autores,)
+function Libro(autor,titulo, editorial, prestados, cantidad, fecha, ) {
+
+} 
+
+function Autor(nombre, fechaNacimiento, , ) {
+    this.nombre = nombre;
+    this.fechaNacimiento = fechaNacimiento;
+}
+
+const miguelCervantes = new Autor("Miguel Cervantes", "1547");
+const victorHugo = new Autor("Victor Hugo", "1547");
