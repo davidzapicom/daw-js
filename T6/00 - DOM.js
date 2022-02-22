c9272ab576aa24aa08013c71d87ccfe22091d213
INFORMATICA\davidzm <davidzapico@davidzapico.com>
2022-02-22 11:02:13 +0100

    22

--- T6/00 - DOM.js
+++ T6/00 - DOM.js
@@ -1,6 +1,6 @@
 function inicio() {
     console.log(document);
-    let processDOMnodes = processDOMnodes();
+    let DOMnodes = processDOMnodes();
     let DOMElements = processDOMelements();
 }
 
@@ -30,8 +30,6 @@ const node_types = [
         firstChild
         lastChild        
 */
-
-let domArrayNodes;
 
 function processDOMnodes() {
     let domArrayNodes = domToArray(document);
@@ -63,11 +61,37 @@ function moveDomNodesToArray(node, domArray, numero) {
     }
 }
 
+/* ELEMENTS
+    rootNode ... document.documentElement
+        children
+        parentElement
+        previousElementSibling
+        nextElementSibling
+        firstElementChild
+        lastElementChild
+*/
+
 function processDOMelements() {
-    domArrayElements = domToArrayElements(document);
+    let domArrayElements = domToArrayElements(document);
     printArrayElements(domArrayElements);
     return domArrayElements;
 }
 
-nuevoParrafo = document.createElement("p");
-nuevoParrafo.inner
+function printArrayElements(array) {
+    console.log(array);
+}
+
+function domToArrayElements(document) {
+    let domArray = [];
+    moveDomElementsToArray(document, domArray, 'E0');
+    return domArray;    
+}
+
+function moveDomElementsToArray(element, domArray, numero) {
+    domArray.push([numero ,element.nodeName, element.id, element.className, element.nodeValue, element]);
+    if (element.children) {
+        for (let i=0; i < element.children.length; ++i){
+            moveDomElementsToArray(element.children.item(i), domArray, numero+'.e'+i.toString().padStart(2,'0'));
+        }
+    }
+}
