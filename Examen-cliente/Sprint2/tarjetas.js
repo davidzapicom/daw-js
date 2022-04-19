1b210754c484f3035bcb00d929f06d5bd8764b43
David Zapico <davidzapico@davidzapico.com>
2022-04-19 10:19:32 +0200

    19

--- Examen-cliente/Sprint2/tarjetas.js
+++ Examen-cliente/Sprint2/tarjetas.js
@@ -1,87 +1,8 @@
-const municipios =
-	[
-		'Allande',
-		'Aller',
-		'Amieva',
-		'Avilés',
-		'Belmonte de Miranda',
-		'Bimenes',
-		'Boal',
-		'Cabrales',
-		'Cabranes',
-		'Candamo',
-		'Cangas de Onís',
-		'Cangas del Narcea',
-		'Caravia',
-		'Carreño',
-		'Caso',
-		'Castrillón',
-		'Castropol',
-		'Coaña',
-		'Colunga',
-		'Corvera de Asturias',
-		'Cudillero',
-		'Degaña',
-		'Franco, El',
-		'Gijón',
-		'Gozón',
-		'Grado',
-		'Grandas de Salime',
-		'Ibias',
-		'Illano',
-		'Illas',
-		'Langreo',
-		'Laviana',
-		'Lena',
-		'Llanera',
-		'Llanes',
-		'Mieres',
-		'Morcín',
-		'Muros de Nalón',
-		'Nava',
-		'Navia',
-		'Noreña',
-		'Onís',
-		'Oviedo',
-		'Parres',
-		'Peñamellera Alta',
-		'Peñamellera Baja',
-		'Pesoz',
-		'Piloña',
-		'Ponga',
-		'Pravia',
-		'Proaza',
-		'Quirós',
-		'Regueras, Las',
-		'Ribadedeva',
-		'Ribadesella',
-		'Ribera de Arriba',
-		'Riosa',
-		'Salas',
-		'San Martín de Oscos',
-		'San Martín del Rey Aurelio',
-		'San Tirso de Abres',
-		'Santa Eulalia de Oscos',
-		'Santo Adriano',
-		'Sariego',
-		'Siero',
-		'Sobrescobio',
-		'Somiedo',
-		'Soto del Barco',
-		'Tapia de Casariego',
-		'Taramundi',
-		'Teverga',
-		'Tineo',
-		'Valdés',
-		'Vegadeo',
-		'Villanueva de Oscos',
-		'Villaviciosa',
-		'Villayón',
-		'Yernes y Tameza']
-
 const layoutHTMLTarjeta = {
 	tipo: 'section',
-	atributos: { name: "tarjeta", },
+	atributos: {
+		name: "tarjeta",
+	},
 	estilos: {
 		backgroundColor: 'lightgrey',
 		display: 'flex',
@@ -92,7 +13,9 @@ const layoutHTMLTarjeta = {
 };
 const layoutHTMLLinea = {
 	tipo: 'div',
-	atributos: { name: "linea" },
+	atributos: {
+		name: "linea"
+	},
 	estilos: {
 		backgroundColor: 'dodgerblue',
 		display: 'flex',
@@ -103,7 +26,9 @@ const layoutHTMLLinea = {
 }
 const layoutHTMLCelda = {
 	tipo: 'div',
-	atributos: { name: 'celda' },
+	atributos: {
+		name: 'celda'
+	},
 	estilos: {
 		backgroundColor: 'whitesmoke',
 		display: 'flex',
@@ -116,8 +41,85 @@ const layoutHTMLCelda = {
 		margin: 'auto'
 	}
 }
-
-
+const municipios = [
+	'Allande',
+	'Aller',
+	'Amieva',
+	'Avilés',
+	'Belmonte de Miranda',
+	'Bimenes',
+	'Boal',
+	'Cabrales',
+	'Cabranes',
+	'Candamo',
+	'Cangas de Onís',
+	'Cangas del Narcea',
+	'Caravia',
+	'Carreño',
+	'Caso',
+	'Castrillón',
+	'Castropol',
+	'Coaña',
+	'Colunga',
+	'Corvera de Asturias',
+	'Cudillero',
+	'Degaña',
+	'Franco, El',
+	'Gijón',
+	'Gozón',
+	'Grado',
+	'Grandas de Salime',
+	'Ibias',
+	'Illano',
+	'Illas',
+	'Langreo',
+	'Laviana',
+	'Lena',
+	'Llanera',
+	'Llanes',
+	'Mieres',
+	'Morcín',
+	'Muros de Nalón',
+	'Nava',
+	'Navia',
+	'Noreña',
+	'Onís',
+	'Oviedo',
+	'Parres',
+	'Peñamellera Alta',
+	'Peñamellera Baja',
+	'Pesoz',
+	'Piloña',
+	'Ponga',
+	'Pravia',
+	'Proaza',
+	'Quirós',
+	'Regueras, Las',
+	'Ribadedeva',
+	'Ribadesella',
+	'Ribera de Arriba',
+	'Riosa',
+	'Salas',
+	'San Martín de Oscos',
+	'San Martín del Rey Aurelio',
+	'San Tirso de Abres',
+	'Santa Eulalia de Oscos',
+	'Santo Adriano',
+	'Sariego',
+	'Siero',
+	'Sobrescobio',
+	'Somiedo',
+	'Soto del Barco',
+	'Tapia de Casariego',
+	'Taramundi',
+	'Teverga',
+	'Tineo',
+	'Valdés',
+	'Vegadeo',
+	'Villanueva de Oscos',
+	'Villaviciosa',
+	'Villayón',
+	'Yernes y Tameza']
 function generarElementoHTML(layout, altura, anchura) {
 	if (layout != undefined) {
 		let elementoHTML = document.createElement(layout.tipo);
@@ -127,77 +129,78 @@ function generarElementoHTML(layout, altura, anchura) {
 		for (let estilo in layout.estilos) {
 			elementoHTML.style[estilo] = layout.estilos[estilo];
 		}
-		elementoHTML.style.height = altura / 5 + "px";
-		elementoHTML.style.width = anchura / 2 + "px";
+		elementoHTML.style["height"] = altura + "px";
+		elementoHTML.style["width"] = anchura + "px";
 		return elementoHTML;
 	}
 	return null;
 }
-
-
 function numeroAleatorio(desde, hasta) {
 	return Math.floor(Math.random() * (hasta - desde + 1) + desde);
 }
-
-
 class Tarjeta {
 	constructor(numeroDeLineas = 5, numeroDeColumnas = 5, alto = 50, ancho = 50, contenido) {
 		this.numeroDeLineas = numeroDeLineas;
 		this.numeroDeColumnas = numeroDeColumnas;
 		this.alto = alto;
 		this.ancho = ancho;
-		let altoTarjeta = alto * numeroDeLineas;
-        let anchoTarjeta = ancho * numeroDeColumnas;
-		this.ElementoHTML = generarElementoHTML(layoutHTMLTarjeta, altoTarjeta, anchoTarjeta);
-		this.lineas = [];
-		this.contenido = [];
+		this.contenido = contenido;
 		this.palabras = [];
-		
+		this.anchoTarjeta = this.numeroDeColumnas * this.ancho;
+		this.altoTarjeta = this.numeroDeLineas * this.alto;
+		this.elementoHTML = generarElementoHTML(layoutHTMLTarjeta);
+		this.lineas = [];
+		this.selecionarContenido();
 		this.generarLineas(alto, ancho);
-		document.body.insertAdjacentElement("beforeend", this.ElementoHTML);
-		seleccionarContenido();
+		document.body.insertAdjacentElement("beforeend", this.elementoHTML);
 	}
-
-	// seleccionarContenido() {
-
-	// }
-
+	selecionarContenido() {
+		while (this.palabras.length < this.numeroDeColumnas) {
+			let n = numeroAleatorio(0, this.contenido.length - 1);
+			if (this.palabras.indexOf(this.contenido[n]) == -1) {
+				this.palabras.push(this.contenido[n]);
+			}
+		}
+	}
 	generarLineas(alto, ancho) {
-		for (let i = 1; i <= this.numeroDeLineas; i++) {
-			let desde = i * this.numeroDeColumnas - (this.numeroDeColumnas - 1);
-			let hasta = i * this.numeroDeColumnas;
-			let linea = new Linea(this, desde, hasta, alto, ancho, this.numeroDeColumnas * ancho, alto);
+		for (let i = 0; i < this.numeroDeLineas; i++) {
+			let datosLinea = [];
+			for (let j = 0; j < this.palabras.length; j++) {
+				if (this.palabras[j].length > i) {
+					datosLinea.push(this.palabras[j].charAt(i));
+				}
+				else {
+					datosLinea.push(" ");
+				}
+			}
+			let linea = new Linea(this, i * this.numeroDeColumnas + 1, (i + 1) * this.numeroDeColumnas, ancho, alto, datosLinea);
 			this.lineas.push(linea);
 		}
 	}
 }
 
-
 class Linea {
-	constructor(tarjeta, desde, hasta, alto, ancho, altoLinea, anchoLinea, datos) {
+	constructor(tarjeta, desde, hasta, alto, ancho, datos) {
 		this.tarjeta = tarjeta;
 		this.desde = desde;
 		this.hasta = hasta;
 		this.alto = alto;
 		this.ancho = ancho;
-		this.altoLinea = altoLinea;
-		this.anchoLinea = anchoLinea;
-		this.ElementoHTML = generarElementoHTML(layoutHTMLLinea, altoLinea, anchoLinea);
+		this.altoLinea = this.alto;
+		this.anchoLinea = this.ancho * (this.hasta - this.desde + 1);
+		this.datos = datos;
+		this.elementoHTML = generarElementoHTML(layoutHTMLLinea, this.altoLinea, this.anchoLinea);
 		this.celdas = [];
-		this.datos = [];
-
 		this.generarCeldas(alto, ancho);
-		document.body.insertAdjacentElement("beforeend", this.ElementoHTML);
+		document.body.insertAdjacentElement("beforeend", this.elementoHTML);
 	}
-
 	generarCeldas(alto, ancho) {
-		for (let i = this.desde; i <= this.hasta; i++) {
-			let celdas = new Celda(this, i, alto, ancho);
-			this.celdas.push(celdas);
+		for (let i = 0; i < this.datos.length; i++) {
+			let celda = new Celda(this, this.datos[i], alto, ancho);
+			this.celdas.push(celda);
 		}
 	}
 }
-
 
 class Celda {
 	constructor(linea, numero, alto, ancho) {
@@ -205,17 +208,11 @@ class Celda {
 		this.numero = numero;
 		this.alto = alto;
 		this.ancho = ancho;
-
-		this.ElementoHTML = generarElementoHTML(layoutHTMLCelda, alto, ancho);
-		if (this.numero == 0) {
-			this.ElementoHTML.innerHTML = "";
-		} else {
-			this.ElementoHTML.innerHTML = this.numero;
-		}
-		this.linea.ElementoHTML.insertAdjacentElement("beforeend", this.ElementoHTML);
+		this.elementoHTML = generarElementoHTML(layoutHTMLCelda, alto, ancho);
+		this.elementoHTML.innerHTML = this.numero;
+		this.linea.elementoHTML.insertAdjacentElement("beforeend", this.elementoHTML);
 	}
 }
-
 
 let miTarjeta3 = new Tarjeta(15, 5, 50, 50, municipios);
 let miTarjeta1 = new Tarjeta(8, 12, 60, 80, municipios);
