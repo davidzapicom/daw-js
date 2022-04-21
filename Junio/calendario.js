2976eecf95280e2dca3d198e60ed551e81d8f36d
David Zapico <davidzapico@davidzapico.com>
2022-04-21 13:51:40 +0200

    21

--- Junio/calendario.js
+++ Junio/calendario.js
@@ -1,31 +1,19 @@
 function obtenerDiasMes(anio, mes) {
-    const dias = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-    if (mes == 1) {
-        if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0) {
-            return dias[1] + 1;
-        } else {
-            return dias[1];
-        }
-    } else {
-        return dias[mes];
-    }
+    const dias = [31, ((anio % 4 == 0) && ((anio % 100 != 0) || (anio % 400) == 0)) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    return dias[mes];
 }
 
-
-
-
-function generarCalendario(mes, anio) {
-    const dias = new Array("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo");
+function generarCalendario(anio, mes) {
+    const dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
     let fecha = new Date(anio, mes, 1);
     let diaSemana = fecha.getDay();
     let aSemana = [];
     let aCalendario = [];
     diaSemana = (diaSemana == 0) ? 7 : diaSemana;
-    let i = 1 
+    let i = 1
     while (i < diaSemana) {
         i++;
-        aSemana.push(" "); 
-
+        aSemana.push(" ");
     }
 
     for (let j = 1; j <= dias.length; j++) {
@@ -33,7 +21,7 @@ function generarCalendario(mes, anio) {
         if (i == 7) {
             aCalendario.push(aSemana);
             aSemana = [];
-            i=0;
+            i = 0;
         }
         i++;
     }
@@ -44,7 +32,6 @@ function generarCalendario(mes, anio) {
         while (i < 7) {
             i++;
             aSemana.push(" ");
-
         }
         aCalendario.push(aSemana);
     }
@@ -56,12 +43,61 @@ function cargarCalendario(evento, objeto) {
     const mes = objeto.mes;
     calendario_abril.elementoHTML.remove();
 
-    let arrayCalendario = obtenerCalendario(anio, mes);
+    let arrayCalendario = generarCalendario(anio, mes);
 
     console.log(arrayCalendario);
     calendario = new Calendario(arrayCalendario, anio, mes);
-
 }
 
+function preparaControlCalendario() {
+    const fecha = new Date();
+    const mesDefecto = fecha.getMonth();
+    const anioDefecto = fecha.getFullYear();
+    const arrayCalendario = generarCalendario(anioDefecto, mesDefecto);
+    const arrayMeses = [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+    ];
 
-let meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+    const arrayAnios = Array.from(Array(1800)).map((e, i) => i + 400);
+    const control = document.getElementById('calendarControl');
+    const formulario = document.createElement('form');
+    const controlAnio = document.createElement('select');
+    const controlMes = document.createElement('select');
+
+    arrayAnios.forEach(anio => {
+        let opcion = document.createElement('option');
+        opcion.value = anio;
+        opcion.text = anio;
+        opcion.selected = anio === anioDefecto;
+        controlAnio.append(opcion);
+    });
+
+    controlAnio.setAttribute('name', 'anio');
+    arrayMeses.forEach((mes, i) => {
+        let opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.text = mes;
+        opcion.selected = i === mesDefecto;
+        controlMes.append(opcion);
+    });
+
+    controlMes.setAttribute('name', 'mes');
+    formulario.append(controlAnio);
+    formulario.append(controlMes);
+    control.append(formulario);
+    control.addEventListener('change', (e) => cargaCalendario(e, formulario));
+    calendario = new Tarjeta(arrayCalendario, 80, 80);
+}
+
+preparaControlCalendario();
