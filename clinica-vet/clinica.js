12206a54533345f8ff108c79e80d4d8f9a2f250d
David Zapico <davidzapico@davidzapico.com>
2022-05-24 10:40:23 +0200

    24 may

--- clinica-vet/clinica.js
+++ clinica-vet/clinica.js
@@ -12,23 +12,46 @@ class Clinica {
 
 class Mascota {
     constructor(nombre, edad, peso) {
-        this.nombre = nombre;
+        this.nombreMascota = nombreMascota;
+        this.nombrePropietario = nombrePropietario;
         this.edad = edad;
         this.peso = peso;
         this.vacunas = [];
-        this.activo = "Active";
+        this.activo = "Active";    
     }
+
     vacunar(nombre, edad, peso) {
-
-        this.vacunas.push(new Mascota(nombre, edad, peso));
+        this.mascotaHTML = document.createElement('div');
+        let nombreHTML = document.createElement('span');
+        if (mascotas.length == 0) {
+            resultado.innerHTML = `No hay ninguna mascota.`;
+        } else {
+            for (var i = 0; i < miClinica.mascotas.length; i++) {
+                if (mascotas[i].activo == "Active") {
+                    nombreHTML.innerHTML = mascotas[i].nombre;
+                    this.mascotaHTML.append(nombreHTML);
+                    botonVacunar = document.createElement('button');
+                    botonVacunar.innerHTML = 'Vacunar';
+                    mascotaHTML.append(botonVacunar);
+                    let pMascota = mascotas[i];
+                    //! botonVacunar.onclick = (e) => registrarVacuna(e, pMascota);
+                }
+                resultado.append(mascotaHTML);
+            }
+        }
+        this.Mascota.vacunas.push(new Vacuna(nombre, edad, peso));
     }
-
 }
 
+
 class Vacuna {
-    constructor(nombre, nombreVacuna, fechaVacuna) {
-        this.nombre = nombre;
+    constructor(nombreVacuna, fechaVacuna) {
         this.nombreVacuna = nombreVacuna;
         this.fechaVacuna = fechaVacuna;
     }
 }
+
+
+
+
+let miClinica = new Clinica('Clinica Veterinaria');
