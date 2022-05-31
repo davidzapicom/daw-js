781a515471ea2e840b136c345fae2d5dec2ea1bd
David Zapico <davidzapico@davidzapico.com>
2022-05-31 21:36:15 +0200

    31 may

--- examen_prueba-rec/Ej2/sorteo.js
+++ examen_prueba-rec/Ej2/sorteo.js
@@ -1,36 +1,41 @@
+const personas = [
+    'Tania Lloret Sonsol',
+    'Thiago Sintra Sintra',
+    'Tomás Porras Puerta',
+    'Violeta Ahmed',
+    'Yanira Domínguez Campoy',
+    'Yassine Paez Polito',
+    'Ana Marín Morán',
+    'Carolina Domínguez Domínguez',
+    'Gerardo Serra Clemente',
+    'Laura Postergado Plinto',
+    'Lucía Álvarez Álvarez',
+    'María Rosa Sinpar Parece',
+    'Noemí García García',
+    'Omar Terreros Marinos',
+    'Sebastián Coto Cotelo',
+    'Umer Mers Sian',
+    'Vanessa Domingo Nieves',
+    'África Martín Martín',
+    'Domingo Bono Cinco',
+    'Javier González Suárez',
+    'Juan Jonil Juanco',
+    'Miguel Fernández Morán',
+    'Paloma Cerco Del Monte',
+    'Raúl Lomas Lomas',
+    'Susana Tomás Martínez',
+    'Abraham Benevente García',
+    'María Rodríguez Pérez'
+];
+
+var liElementoHTML = [];
+
 class Sorteo {
-    constructor(nombre) {
+    constructor(nombre, personas) {
         this.nombre = nombre;
         this.numsAl = [];
-        this.personas = [
-            'Tania Lloret Sonsol',
-            'Thiago Sintra Sintra',
-            'Tomás Porras Puerta',
-            'Violeta Ahmed',
-            'Yanira Domínguez Campoy',
-            'Yassine Paez Polito',
-            'Ana Marín Morán',
-            'Carolina Domínguez Domínguez',
-            'Gerardo Serra Clemente',
-            'Laura Postergado Plinto',
-            'Lucía Álvarez Álvarez',
-            'María Rosa Sinpar Parece',
-            'Noemí García García',
-            'Omar Terreros Marinos',
-            'Sebastián Coto Cotelo',
-            'Umer Mers Sian',
-            'Vanessa Domingo Nieves',
-            'África Martín Martín',
-            'Domingo Bono Cinco',
-            'Javier González Suárez',
-            'Juan Jonil Juanco',
-            'Miguel Fernández Morán',
-            'Paloma Cerco Del Monte',
-            'Raúl Lomas Lomas',
-            'Susana Tomás Martínez',
-            'Abraham Benevente García',
-            'María Rodríguez Pérez'
-        ];
+        this.personas = personas;
+
         document.title = this.nombre;
 
         //* DIV CONTENEDOR
@@ -95,6 +100,7 @@ class Sorteo {
         document.body.append(this.sorteo);
     }
 
+
     asignarPersonas(e) {
         this.btnPreparar.disabled = true;
 
@@ -122,6 +128,7 @@ class Sorteo {
             let element = this.numsAl[i];
 
             this.li.append(`${persona}. Número aleatorio asignado:  ${element}`);
+            liElementoHTML.push(this.li);
             this.ol.append(this.li);
         }
         this.btnSortear.style.display = 'block';
@@ -132,15 +139,15 @@ class Sorteo {
         this.btnSortear.disabled = true;
         this.anotacion.innerHTML = "SORTEANDO";
         let numPremiado = Math.floor(Math.random() * (this.personas.length - 1)) + 1;
-        let tempo = setTimeout(function () {
-            this.anotacion.innerHTML = "";
-        }, 3000);            
-
+        // let tempo = setTimeout(function () {
+        //     this.anotacion.innerHTML = "";
+        // }, 3000);            
+        
         let pos = this.numsAl.findIndex(ele => ele === numPremiado);
-        pos+=1;
-        this.anotacion.replaceWith(`La persona ganadora es ${this.personasOrdenado[pos-1]} con la posición ${pos} y el numero premiado: ${numPremiado}`);
+        liElementoHTML[pos].style.backgroundColor = '#f00';
+        this.anotacion.replaceWith(`La persona ganadora es ${this.personasOrdenado[pos]} con la posición ${pos+=1} y el numero premiado ${numPremiado}`);
 
     }
 }
 
-let miSorteo = new Sorteo('Sorteos Divinos');
+let miSorteo = new Sorteo('Sorteos Divinos', personas);
