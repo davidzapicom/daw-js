fd6c52d659aa47c1cffada126fada9971b54a428
David Zapico <davidzapico@davidzapico.com>
2022-06-01 12:47:37 +0200

    1 jun

--- Ejemplo-examen/examenVentas.php
+++
@@ -1,9 +0,0 @@
-<?php
-	//Adaptada a Español
-	header('Content-Type: text/html;charset=utf-8');
-
-	$id = $_POST["id"];
-	$cantidad = $_POST["cantidad"];
-
-	echo "Vendidas ". $cantidad. " del articulo ". $id;
-?>
